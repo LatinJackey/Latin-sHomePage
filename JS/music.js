0b6136521cb1247695ee7f1033398553388d89b8
song <song@txzkeji.com>
2018-02-26 16:50:49 +0800

    add video

--- JS/music.js
+++ JS/music.js
@@ -2,13 +2,11 @@
 
 var obj ={
     Play :function(){
-        console.log("heasdf");
         var player = document.getElementById("music");
         player.play();
     },
     
     Pause :function (){
-        console.log("heasdf");
         var player = document.getElementById("music");
         player.pause();
     },
