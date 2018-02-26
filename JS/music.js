3dff744941f51b869ce10cd543b1043e65211c38
song <song@txzkeji.com>
2018-02-26 17:27:01 +0800

    l

--- JS/music.js
+++ JS/music.js
@@ -1,24 +1,23 @@
 
 
 var obj ={
-    Play :function(){
-        var player = document.getElementById("music");
-        player.play();
+    ChangeStatus :function(){
+        var audio = document.getElementById("music");
+        if (audio.paused){
+            audio.play();
+            document.getElementById("ChangeStatus").style.backgroundImage="url('./images/暂停播放.png')";
+        }else{
+            audio.pause();
+            document.getElementById("ChangeStatus").style.backgroundImage="url('./images/开始播放.png')";
+        }
     },
-    
-    Pause :function (){
-        var player = document.getElementById("music");
-        player.pause();
-    },
-    
     Next :function (){
         MenuClick();
     },
 }
 
 window.onload = function () {
-    document.getElementById("Pause").onclick = obj.Pause;
-    document.getElementById("Play").onclick = obj.Play;
+    document.getElementById("ChangeStatus").onclick = obj.ChangeStatus;
     document.getElementById("Next").onclick = obj.Next;
 }
 
