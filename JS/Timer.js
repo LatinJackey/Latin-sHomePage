63c2a40919e0483148bf9c5e040b0cc37b9993ea
LatinJackey <87328375@qq.com>
2018-03-08 21:10:58 +0800

    日期bug修复

--- JS/Timer.js
+++ JS/Timer.js
@@ -18,8 +18,9 @@ var MyTimer={
     getCurrentDate:function(){
         var currentDate = new Date ();//获取当前时间对象
         var year = currentDate.getFullYear();//当前年
-        var month = currentDate.getMonth();//当前月
-        var day = currentDate.getDay();//当前日
+        var month = currentDate.getMonth() + 1;//当前月
+        var day = currentDate.getDate();//当前日
+        console.log('hello',month,day);
 
         var hours = MyTimer.changeDou(currentDate.getHours());//当前小时 格式:00
         var minutes = MyTimer.changeDou(currentDate.getMinutes());//当前分钟 格式:00
